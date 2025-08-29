b75998a7f194020f442dbdce9d33dbab702596fe
wangqiwen <wangqiwen@qq.com>
2025-08-29 17:29:36 +0800

    update the 26 lesson files

--- 26-code-structuring-for-bigger-projects/src/script.js
+++ 26-code-structuring-for-bigger-projects/src/script.js
@@ -2,7 +2,10 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import Experience from './Experience/Experience.js'
 
+
+const experience = new Experience()
 /**
  * Loaders
  */
