3d317231127c72e1934b90bfad33a9a81a86e9b0
wangqiwen <wangqiwen@qq.com>
2025-09-10 19:03:14 +0800

    update the 26 lesson files

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -1,9 +1,19 @@
 import * as THREE from 'three'
 import Sizes from './Utils/Size.js'
 import Time from './Utils/Time.js'
-import Camera from './Camera.js'
+import Camera from './Utils/Camera.js'
+import Renderer from './Renderer.js'
+
+let instance = null
 export default class Experience {
   constructor(canvas) {
+
+    // Singleton
+    if(instance) {
+      return instance
+    }
+    instance = this
+    
     // Global access
     window.experience = this
 
@@ -15,7 +25,8 @@ export default class Experience {
     this.sizes = new Sizes()
     this.time = new Time()
     this.scene = new THREE.Scene()
-    this.camera = new Camera(this)
+    this.camera = new Camera()
+    this.renderer = new Renderer()
 
     // Resize event
     this.sizes.on('resize', () => {
@@ -28,10 +39,13 @@ export default class Experience {
   }
 
   resize() {
-
+    this.camera.resize()
+    this.renderer.resize()
   }
 
   update() {
-
+    // console.log('time tick resolve ---->')
+    this.camera.update()
+    this.renderer.update()
   }
 }
