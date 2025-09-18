51a6fb01438499e5701c388e060cfc627703aa76
wangqiwen <wangqiwen@qq.com>
2025-09-18 16:19:11 +0800

    update the 26 lesson files

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -1,3 +1,4 @@
+import Debug from './Utils/Debug.js'
 import * as THREE from 'three'
 import Sizes from './Utils/Sizes.js'
 import Time from './Utils/Time.js'
@@ -24,6 +25,7 @@ export default class Experience {
     this.canvas = canvas
 
     // Setup
+    this.debug = new Debug()
     this.sizes = new Sizes()
     this.time = new Time()
     this.scene = new THREE.Scene()
@@ -51,6 +53,37 @@ export default class Experience {
 
   update() {
     this.camera.update()
+    this.world.update()
     this.renderer.update()
+  }
+
+  destroy() {
+    this.sizes.off('resize')
+    this.time.off('tick')
+
+    // Traverse the whole scene
+    this.scene.traverse((child) => {
+      // Test if it's a mesh
+      if(child instanceof THREE.Mesh) {
+        child.geometry.dispose()
+
+        // Loop through the material properties
+        for(const key in child.material) {
+          const value = child.material[key]
+
+          // Test if there is a dispose function
+          if(value && typeof value.dispose === 'function') {
+            value.dispose()
+          }
+        }
+      }
+    })
+
+    this.camera.controls.dispose()
+    this.renderer.instance.dispose()
+
+    if(this.debug.active) {
+      this.debug.ui.destroy()
+    }
   }
 }
--- 26-code-structuring-for-bigger-projects/src/Experience/World/World.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/World/World.js
@@ -1,25 +1,25 @@
 import * as THREE from "three"
 import Experience from "../Experience.js";
 import Environment from "./Environment.js";
-
+import Floor from './Floor.js'
+import Fox from './Fox.js'
 export default class World {
   constructor() {
     this.experience = new Experience()
     this.scene = this.experience.scene
     this.resources = this.experience.resources
 
-    // Test mesh
-    const testMesh = new THREE.Mesh(
-      new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshStandardMaterial()
-    )
-
     // Wait for resources
     this.resources.on('ready', () => {
       // Setup
+      this.floor = new Floor()
+      this.fox = new Fox()
       this.environment = new Environment()
     })
-
-    this.scene.add(testMesh)
+  }
+  update() {
+    if(this.fox) {
+      this.fox.update()
+    }
   }
 }
