3de38d557f31b74856779735dfb349b41bbf3786
wangqiwen <wangqiwen@qq.com>
2025-11-07 16:59:00 +0800

    finally the 57 lesson

--- 57-debug-a-r3f-application/src/Experience.jsx
+++ 57-debug-a-r3f-application/src/Experience.jsx
@@ -1,17 +1,51 @@
 import { OrbitControls } from '@react-three/drei'
+import { button, useControls } from 'leva'
+import { Perf } from "r3f-perf";
 
 export default function Experience()
 {
+    const { position, color, visible } = useControls('sphere', {
+        position: {
+            value: { x: - 2, y: 0 },
+            step: 0.01,
+            joystick: 'invertY',
+        },
+        color: '#ff0000',
+        visible: true,
+        myInterval: {
+            min: 0,
+            max: 10,
+            value: [4, 5]
+        },
+        clickMe: button(() => { console.log('ok') }),
+        choice: { options: [ 'a', 'b', 'c']}
+    })
+
+    const { scale } = useControls('cube', {
+        scale: {
+            value: 1.5,
+            step: 0.01,
+            min: 0,
+            max: 5
+        }
+    })
+
+    const { perfVisible } = useControls({
+        perfVisible: true
+    })
+
     return <>
+
+        { perfVisible && <Perf position="top-left" />}
 
         <OrbitControls makeDefault />
 
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
         <ambientLight intensity={ 1.5 } />
 
-        <mesh position-x={ - 2 }>
+        <mesh visible={ visible } position={ [ position.x, position.y, 0 ] }>
             <sphereGeometry />
-            <meshStandardMaterial color="orange" />
+            <meshStandardMaterial color={ color } />
         </mesh>
 
         <mesh position-x={ 2 } scale={ 1.5 }>
