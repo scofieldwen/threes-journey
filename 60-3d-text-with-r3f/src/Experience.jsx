22085bd667c0836d1b98f6c3276115b063a3c8da
wangqiwen <wangqiwen@qq.com>
2025-11-28 17:34:39 +0800

    update the 60 lesson files

--- 60-3d-text-with-r3f/src/Experience.jsx
+++ 60-3d-text-with-r3f/src/Experience.jsx
@@ -1,18 +1,75 @@
-import { OrbitControls } from '@react-three/drei'
+import * as THREE from "three";
+import { useMatcapTexture, Center, Text3D, OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
+import { useRef, useEffect, useState } from "react";
+import { useFrame } from '@react-three/fiber'
+
+const torusGeometry = new THREE.TorusGeometry(1, 0.6, 16, 32)
+const material = new THREE.MeshMatcapMaterial()
 
 export default function Experience()
 {
+    const dountGroup = useRef()
+    // const [ torusGeometry, setTorusGeometry] = useState()
+    // const [ material, setMaterial ] = useState()
+
+    const [ matcapTexture ] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256)
+
+    useEffect(() => {
+        matcapTexture.colorSpace = THREE.SRGBColorSpace
+        matcapTexture.needsUpdate = true
+
+        material.matcap = matcapTexture
+        material.needsUpdate = true
+    }, [])
+
     return <>
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
 
-        <mesh scale={ 1.5 }>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+        <Center>
+            <Text3D
+                material={ material }
+                font="./fonts/helvetiker_regular.typeface.json"
+                size={ 0.75 }
+                height={ 0.2 }
+                curveSegments={ 12 }
+                bevelEnabled
+                bevelThickness={ 0.02 }
+                bevelSize={ 0.02 }
+                bevelOffset={ 0 }
+                bevelSegments={ 5 }
+            >
+                HELLO R3F
+                <meshMatcapMaterial matcap={ matcapTexture } />
+            </Text3D>
+        </Center>
 
+        {/* <torusGeometry ref={ setTorusGeometry } args={ [ 1, 0.6, 16, 32 ] } />
+        <meshMatcapMaterial ref={ setMaterial } matcap={ matcapTexture } /> */}
+
+        <group ref={ dountGroup }>
+            { [...Array(100)].map((value, index) =>
+                <mesh
+                    key={ index }
+                    geometry={ torusGeometry }
+                    material={ material }
+                    position={ [
+                        (Math.random() - 0.5) * 10,
+                        (Math.random() - 0.5) * 10,
+                        (Math.random() - 0.5) * 10
+                    ] }
+                    scale={ 0.2 + Math.random() * 0.2 }
+                    rotation={ [
+                        Math.random() * Math.PI,
+                        Math.random() * Math.PI,
+                        0
+                    ] }
+                >
+                </mesh>
+            ) }
+        </group>
     </>
 }
